ef5cbc1bb42f352bae27370cbffc4e0d9ecd515b
Jim Martens <jma@hbt.de>
2024-08-30 14:42:06 +0200

    fix: Removed unsupported annotations

--- module-support/src/main/kotlin/de/hbt/support/property/StatusProbeProperties.kt
+++ module-support/src/main/kotlin/de/hbt/support/property/StatusProbeProperties.kt
@@ -4,11 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.bind.ConstructorBinding
 import org.springframework.boot.convert.DurationUnit
-import org.springframework.cloud.context.config.annotation.RefreshScope
 import java.time.Duration
 import java.time.temporal.ChronoUnit
 
-@RefreshScope
 @ConfigurationProperties(prefix = "de.hbt.routing.statusprobe")
 @Schema(description = "Properties, to configure this Application")
 data class StatusProbeProperties @ConstructorBinding constructor(
