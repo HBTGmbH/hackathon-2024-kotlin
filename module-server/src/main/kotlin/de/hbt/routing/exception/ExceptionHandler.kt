f04ded48a0a302551c7d8657f8cd90938dbe6f32
Jim Martens <github@2martens.de>
2024-09-18 14:21:03 +0200

    Fixed exception handling

--- module-server/src/main/kotlin/de/hbt/routing/exception/ExceptionHandler.kt
+++ module-server/src/main/kotlin/de/hbt/routing/exception/ExceptionHandler.kt
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 class ExceptionHandler {
     @ExceptionHandler
     fun handleChatException(e: ChatException): ResponseEntity<AppController.ErrorMessage> {
-        val errorMessage = AppController.ErrorMessage(AppController.Message(requestId = e.requestId,
-                content = e.localizedMessage, role = "assistant"))
+        val errorMessage = AppController.ErrorMessage(requestId = e.requestId,
+                content = e.localizedMessage)
 
         return ResponseEntity(errorMessage, e.status)
     }
