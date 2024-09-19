85e377108d83bd1b871105409d9217de043d73e9
Jim Martens <github@2martens.de>
2024-09-19 08:46:25 +0200

    Move dependency to version catalog

--- module-server/server.gradle.kts
+++ module-server/server.gradle.kts
@@ -13,6 +13,5 @@ dependencies {
     kapt(libs.mapstruct.processor)
 
     implementation(libs.bundles.spring.boot.security)
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.+")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.+")
+    implementation(libs.bundles.jackson)
 }
