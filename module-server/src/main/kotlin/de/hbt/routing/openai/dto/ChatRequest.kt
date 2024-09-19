a7b03f13730ee681775aab176d1f93c7243538cc
sku <sku@hbt.de>
2024-09-19 08:43:57 +0200

    Update ChatRequest default model to the cheaper 'gpt-4o-mini'

--- module-server/src/main/kotlin/de/hbt/routing/openai/dto/ChatRequest.kt
+++ module-server/src/main/kotlin/de/hbt/routing/openai/dto/ChatRequest.kt
@@ -3,7 +3,7 @@ package de.hbt.routing.openai.dto
 data class ChatRequest(val model: String, val n: Int, val temperature: Double, val messages: MutableList<Message>) {
 
     companion object {
-        private const val MODEL = "gpt-4o"
+        private const val MODEL = "gpt-4o-mini"
         fun chatRequest(systemPrompt: String, userPrompt: String): ChatRequest {
             val messages = ArrayList<Message>()
             messages.add(Message("system", systemPrompt))
